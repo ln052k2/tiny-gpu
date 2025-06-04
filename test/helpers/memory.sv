b92d288b61513d9a517c769f66c216591922c041
ln052k2 <118166457+ln052k2@users.noreply.github.com>
2025-06-04 16:57:43 -0700

    Memory helper module no longer uses an interface -- may be readded in the future

--- test/helpers/memory.sv
+++ test/helpers/memory.sv
@@ -10,64 +10,63 @@
 // - Write ready
 // - Write address
 // - Write data
-interface dut_if #(parameter ADDR_BITS = 8, DATA_BITS = 16, CHANNELS = 4);
-    logic [CHANNELS-1:0] mem_read_valid;
-    logic [ADDR_BITS-1:0] mem_read_address [CHANNELS];
-    logic [DATA_BITS-1:0] mem_read_data [CHANNELS];
-    logic [CHANNELS-1:0] mem_read_ready;
-
-    logic [CHANNELS-1:0] mem_write_valid;
-    logic [ADDR_BITS-1:0] mem_write_address [CHANNELS];
-    logic [DATA_BITS-1:0] mem_write_data [CHANNELS];
-    logic [CHANNELS-1:0] mem_write_ready;
-endinterface
 
 // Memory class
 class Memory;
 string name;
-virtual dut_if #(ADDR_BITS, DATA_BITS, CHANNELS) dut;
+
+logic [CHANNELS-1:0] mem_read_valid;
+logic [ADDR_BITS-1:0] mem_read_address [CHANNELS];
+logic [DATA_BITS-1:0] mem_read_data [CHANNELS];
+logic [CHANNELS-1:0] mem_read_ready;
+
+logic [CHANNELS-1:0] mem_write_valid;
+logic [ADDR_BITS-1:0] mem_write_address [CHANNELS];
+logic [DATA_BITS-1:0] mem_write_data [CHANNELS];
+logic [CHANNELS-1:0] mem_write_ready;
 
 typedef logic [ADDR_BITS-1:0] addr_t;
 typedef logic [DATA_BITS-1:0] data_t;
 data_t memory [0:2**ADDR_BITS-1];
 
-function new(string name__, virtual dut_if vif__);
-    name = name__;
-    dut = vif__;
-    foreach (memory[i]) memory[i] = '0; // Initialize memory to zero
+function new(string name__);
+    this.name = name__;
+    // Initialize memory to zero
+    foreach (memory[i]) memory[i] = '0;
 endfunction
 
+// Run function:
 // Run function:
 task run();
     data_t read_data[CHANNELS];
     bit read_ready[CHANNELS];
     bit write_ready[CHANNELS];
 
-    // Read valid and read address do not need to be cast/converted
+    // READ logic
     for (int i = 0; i < CHANNELS; i++) begin
-        if (dut.mem_read_valid[i]) begin
-            read_data[i]  = memory[dut.mem_read_address[i]];
+        if (mem_read_valid[i]) begin
+            read_data[i]  = memory[mem_read_address[i]];
             read_ready[i] = 1;
         end else begin
-            // Read ready and read data are both initialized to 0
             read_data[i]  = '0;
             read_ready[i] = 0;
         end
-        dut.mem_read_data[i]  = read_data[i];
-        dut.mem_read_ready[i] = read_ready[i];
+        mem_read_data[i]  = read_data[i];
+        mem_read_ready[i] = read_ready[i];
     end
 
     // WRITE logic
     for (int i = 0; i < CHANNELS; i++) begin
-        if (dut.mem_write_valid[i]) begin
-            memory[dut.mem_write_address[i]] = dut.mem_write_data[i];
+        if (mem_write_valid[i]) begin
+            memory[mem_write_address[i]] = mem_write_data[i];
             write_ready[i] = 1;
         end else begin
             write_ready[i] = 0;
         end
-        dut.mem_write_ready[i] = write_ready[i];
+        mem_write_ready[i] = write_ready[i];
     end
 endtask
+
 
 // Write function (bypasses the run function)
 function void write(addr_t address, data_t data);
