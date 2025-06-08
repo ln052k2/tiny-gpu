871d23c3723dd49c493facf76a450106be91e174
ln052k2 <118166457+ln052k2@users.noreply.github.com>
2025-06-07 19:12:32 -0700

    Change test_matadd to instantiate DUT with mem_if

--- test/test_matadd.sv
+++ test/test_matadd.sv
@@ -95,20 +95,10 @@ module test_matadd;
         .device_control_data(device_control_data),
 
         // Program memory hookup via interface
-        .program_mem_read_valid(program_mem_if.read_valid),
-        .program_mem_read_address(program_mem_if.read_address),
-        .program_mem_read_ready(program_mem_if.read_ready),
-        .program_mem_read_data(program_mem_if.read_data),
+        .program_mem_if(program_mem_if),
 
         // Data memory hookup via interface
-        .data_mem_read_valid(data_mem_if.read_valid),
-        .data_mem_read_address(data_mem_if.read_address),
-        .data_mem_read_ready(data_mem_if.read_ready),
-        .data_mem_read_data(data_mem_if.read_data),
-        .data_mem_write_valid(data_mem_if.write_valid),
-        .data_mem_write_address(data_mem_if.write_address),
-        .data_mem_write_data(data_mem_if.write_data),
-        .data_mem_write_ready(data_mem_if.write_ready)
+        .data_mem_if(data_mem_if),
     );
 
     // always @(posedge clk) begin
