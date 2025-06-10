96d54b93cbd3ae19b50f7082c0aeccbff411fdd6
kay <kayhyn@pdx.edu>
2025-06-10 10:31:15 -0700

    divider works and extra files added

--- sources.f
+++ sources.f
@@ -1,4 +1,15 @@
+src/pkg/alu_ops_pkg.sv
+src/pkg/alu_state_pkg.sv
+src/pkg/controller_states_pkg.sv
+src/pkg/core_states_pkg.sv
+src/pkg/fetcher_states_pkg.sv
+src/pkg/lsu_states_pkg.sv
+src/pkg/opcodes_pkg.sv
+
+src/assertions/divider_assertions.sv
+
 src/alu.sv
+src/alu_components/divider.sv
 src/controller.sv
 src/core.sv
 src/dcr.sv
