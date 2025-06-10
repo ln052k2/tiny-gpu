9fb3d06119132311e4f0ba29a9159a5373fa82ff
Lynn Nguyen <ln05.2k2@gmail.com>
2025-06-10 01:46:29 -0700

    Working test_matadd

--- src/memoryif.sv
+++ src/memoryif.sv
@@ -4,13 +4,13 @@ interface mem_if #(
     parameter int CHANNELS  = 1
 );
     logic [CHANNELS-1:0]         read_valid;
-    logic [ADDR_BITS-1:0]        read_address [CHANNELS];
-    logic [DATA_BITS-1:0]        read_data    [CHANNELS];
+    logic [ADDR_BITS-1:0]        read_address [CHANNELS-1:0];
+    logic [DATA_BITS-1:0]        read_data    [CHANNELS-1:0];
     logic [CHANNELS-1:0]         read_ready;
 
     logic [CHANNELS-1:0]         write_valid;
-    logic [ADDR_BITS-1:0]        write_address [CHANNELS];
-    logic [DATA_BITS-1:0]        write_data    [CHANNELS];
+    logic [ADDR_BITS-1:0]        write_address [CHANNELS-1:0];
+    logic [DATA_BITS-1:0]        write_data    [CHANNELS-1:0];
     logic [CHANNELS-1:0]         write_ready;
 
     modport consumer (
